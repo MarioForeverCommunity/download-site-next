23bc0093756fb0710dcab15be8d02aacc26b9f3a
Kevin Huang <huangyf9883@gmail.com>
2024-10-14 20:53:58 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,22 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run build
+# 设置工作目录和分支
+WORK_DIR=/data/mfdownload-next
+BRANCH=main # 根据需要修改分支名
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+# 进入工作目录
+cd $WORK_DIR || exit
+
+# 拉取最新代码
+git pull origin $BRANCH
+
+# 安装依赖和构建
+npm install
+npm run build
 
 # 拷贝到本地 nginx 文件夹下面 \cp 是覆盖源文件
 echo "**************************** 本地部署 *************************"
-rsync -av --ignore-times --delete ./ /data/wwwroot/smwp.marioforever.net/
-chown -R www-data:www-data /data/wwwroot/smwp.marioforever.net/
+rsync -av --ignore-times --delete dist/ /data/wwwroot/download.marioforever.net/new/
+chown -R www-data:www-data /data/wwwroot/download.marioforever.net/
 echo "**************************** 部署成功 *************************"
-
-cd -
-rm -rf docs/.vuepress/dist
