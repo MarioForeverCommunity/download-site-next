defa7540b5a43a6e81291106504beba243958f9a
Kevin Huang <huangyf9883@gmail.com>
2024-10-14 21:35:14 +0800

    Fix

--- src/util/ReadList.js
+++ src/util/ReadList.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import yaml from "js-yaml";
 
 export async function readList(fname) {
-    const res = await axios.get(`/lists/${fname}`);
+    const res = await axios.get(`./lists/${fname}`);
     return await yaml.load(res.data);
 }
