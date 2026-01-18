c26718eb3072021161e5385d6f3508dfbd2a322c
Kevin Huang <huangyf9883@gmail.com>
2026-01-19 02:56:42 +0800

    feat(GameLine): Add tooltip to author links

--- src/components/GameLine.vue
+++ src/components/GameLine.vue
@@ -121,9 +121,14 @@
     <div class="game-author">
       <span v-if="typeof getAuthorList(game, lan) == 'string'">
         <template v-if="getAuthorFolderURL(game, getAuthorList(game, lan), lan)">
-          <a :href="getAuthorFolderURL(game, getAuthorList(game, lan), lan)" target="_blank" rel="noreferrer">
-            {{ getAuthorList(game, lan) }}
-          </a>
+          <Tooltip>
+            <a :href="getAuthorFolderURL(game, getAuthorList(game, lan), lan)" target="_blank" rel="noreferrer">
+              {{ getAuthorList(game, lan) }}
+            </a>
+            <template #popper>
+              {{ lan == 'en' ? ('View all games by ' + getAuthorList(game, lan)) : ('查看 ' + getAuthorList(game, lan) + ' 所有作品') }}
+            </template>
+          </Tooltip>
         </template>
         <template v-else>
           {{ getAuthorList(game, lan) }}
@@ -133,9 +138,14 @@
         <span v-for="(author, authorindex) in getAuthorList(game, lan)" :key="author + authorindex">
           <br v-if="authorindex != 0" />
           <template v-if="getAuthorFolderURL(game, author, lan)">
-            <a :href="getAuthorFolderURL(game, author, lan)" target="_blank" rel="noreferrer">
-              {{ author }}
-            </a>
+            <Tooltip>
+              <a :href="getAuthorFolderURL(game, author, lan)" target="_blank" rel="noreferrer">
+                {{ author }}
+              </a>
+              <template #popper>
+                {{ lan == 'en' ? ('View all games by ' + author) : ('查看 ' + author + ' 所有作品') }}
+              </template>
+            </Tooltip>
           </template>
           <template v-else>
             {{ author }}
