c83ed939fad697f77a165cf97db6a7c7cc7cb834
Kevin Huang <huangyf9883@gmail.com>
2026-08-16 11:57:25 +0800

    Update SmwpVersions.js

--- src/util/SmwpVersions.js
+++ src/util/SmwpVersions.js
@@ -26,5 +26,7 @@ export const SmwpVersions = {
   "v1.7.11": "smwp-1.7.11.7z",
   "v1.7.12": "smwp-1.7.12.7z",
   "v1.7.12.1": "smwp-1.7.12.1.7z",
-  "v1.7.13": "smwp-1.7.13-beta1.7z",
+  "v1.7.12.2": "smwp-1.7.12.2.7z",
+  "v1.7.13-beta.1": "smwp-1.7.13-beta1.7z",
+  "v1.8.0-beta.1": "smwp-1.8.0-beta.1.7z",
 }
